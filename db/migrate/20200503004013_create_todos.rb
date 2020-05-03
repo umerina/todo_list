366cc8a90f922ab2f0027b6ab08f01ba854eb37e
umerina <61220531+umerina@users.noreply.github.com>
2020-05-03 09:50:52 +0900

    migrationfail

--- db/migrate/20200503004013_create_todos.rb
+++ db/migrate/20200503004013_create_todos.rb
@@ -1,7 +1,10 @@
 class CreateTodos < ActiveRecord::Migration[5.0]
   def change
     create_table :todos do |t|
-      t.
+      t.text :task
+      t.text :priority
+      t.date :limit_date
+      t.text :state
       t.timestamps
     end
   end
