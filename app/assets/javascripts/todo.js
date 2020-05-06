d0a275f7ce344c730e5b8552f355f39aeddb919f
umerina <61220531+umerina@users.noreply.github.com>
2020-05-06 11:24:22 +0900

    todo.js/jquery

--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -1,6 +1,22 @@
-$(function(){
-  $('#main__indicate__matter__content1').on('submit', function(e){
-    console.log('hoge');
-    e.preventDefault()
+$('.js-form').on('submit', function(e) {
+  e.preventDefault();
+  var todo = $('.js-form__text-field').val();
+  $.ajax({
+    type: 'POST',
+    url: '/todos.json',
+    data: {
+      todo: {
+        content: todo
+      }
+    },
+    dataType: 'json'
+  })
+  .done(function(data) {
+    var html = $('main__indicate__matter__content1').append(data.content);
+    $('.todos').append(html);
+    $('.js-form__text-field').val('');
+  })
+  .fail(function() {
+    alert('error');
   });
 });
